aab84e3c93b15885af5bffb439157b7e7f0e9afc
willythesillyone <ppin4147@gmail.com>
2025-12-03 01:34:30 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY 19TH BIRTHDAY LAKSMI').split('')
+    const titles = ('Selamat Demis, Giselle Emmanuela Rumimper').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
